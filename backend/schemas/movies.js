c822411e1a261a7b92504545cd7bda6e04079755
dracudev <andreujavier99@gmail.com>
2025-01-10 21:37:18 +0100

    refactor logic into MVC

--- backend/schemas/movies.js
+++ backend/schemas/movies.js
@@ -1,4 +1,4 @@
-const z = require("zod");
+import z from "zod";
 
 const movieSchema = z.object({
   title: z.string({
@@ -13,12 +13,10 @@ const movieSchema = z.object({
   genre: z.array(z.enum(["Action", "Adventure"])),
 });
 
-function validateMovie(object) {
+export function validateMovie(object) {
   return movieSchema.safeParse(object);
 }
 
-function validatePartialMovie(input) {
+export function validatePartialMovie(input) {
   return movieSchema.partial().safeParse(input);
 }
-
-module.exports = { validateMovie, validatePartialMovie };
