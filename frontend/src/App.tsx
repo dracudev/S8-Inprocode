c5ff0e9950673e731b473f0194d391ff4cace288
dracudev <andreujavier99@gmail.com>
2025-01-20 13:14:27 +0100

    [FRONTEND] - Added shadcn sidebar and fixed @ imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
 import "./App.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
-function App() {
-  return (
-    <>
-      <p className="text-white ">App</p>
-    </>
-  );
+interface AppProps {
+  children: React.ReactNode;
 }
+
+const App: React.FC<AppProps> = ({ children }) => {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+};
 
 export default App;
---
+++ frontend/src/components/app-sidebar.tsx
@@ -0,0 +1,67 @@
+import {
+  Calendar,
+  Home,
+  LucideSquareGanttChart,
+  LucideMapPinned,
+} from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Chart",
+    url: "#",
+    icon: LucideSquareGanttChart,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Map",
+    url: "#",
+    icon: LucideMapPinned,
+  },
+];
+export function AppSidebar() {
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarContent className="bg-zinc-900 text-white">
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-white">
+            Inprocode
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild className="text-white">
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
